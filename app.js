c840cdba7d74dde7ed82ea1c4cf0e4adf6eee9dc
Luis Sarmiento <luis@DESKTOP-RYZEN-LUIS.>
2024-07-01 18:07:48 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+import express from 'express'
+import connection from './database/db.js'
+import router from './routes/main.route.js'
+import { constants } from './services/utils/constants.js'
+import { not_found } from './controllers/main.controller.js'
+
+const app = express()
+const port = process.env.PORT
+
+app.use(express.json())
+
+await connection()
+
+app.use('/api', router)
+
+app.get('*', not_found)
+
+app.listen(port, () => console.log(`Server on port: ${port}`))
---
+++ routes/main.route.js
@@ -0,0 +1,20 @@
+import { Router } from "express";
+import { test } from "../controllers/main.controller.js";
+import auth_router from "./auth.route.js";
+import { valid_token } from "../services/middleware/valid-token.js";
+
+const router = Router()
+
+router.get('/test', test)
+
+router.use('/auth', auth_router)
+
+
+const contro = (req, res) => {
+    res.status(200).json({
+        msg: 'Tu score es 1000'
+    })
+}
+router.get('/score', valid_token, contro)
+
+export default router
